cde7fef946d96da7a8373be199cefedde34ef2ec
Marcelo <marcelotc1202@gmail.com>
2026-06-19 14:23:02 -0300

    Update setup.py to include package data
    Added package data to include JSON files in the package.

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ setup(
     name='moriarty',
     version='1.0.0',
     description='OSINT Framework para busca de informações públicas',
-    packages=find_packages(), # Encontra automaticamente a pasta 'src' porque ela tem o __init__.py
+    packages=find_packages(), 
+    package_data={
+        'src': ['data/*.json'], # Força a inclusão do JSON no pacote
+    },
+    include_package_data=True,
     install_requires=[
         'httpx',
         'rich'
     ],
     entry_points={
         'console_scripts': [
-            'moriarty=src.main:main', # Aponta para o arquivo dentro de src e chama a função main()
+            'moriarty=src.main:main', 
         ],
     },
 )
