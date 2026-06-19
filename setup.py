a2da90232f14dcae600134bb78ef4bc51757b306
Marcelo <marcelotc1202@gmail.com>
2026-06-19 14:14:02 -0300

    Refactor setup.py to use find_packages

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='moriarty',
     version='1.0.0',
     description='OSINT Framework para busca de informações públicas',
-    py_modules=['main', 'motor'], # Aqui listamos os arquivos que fazem o projeto rodar
+    packages=find_packages(), # Encontra automaticamente a pasta 'src' porque ela tem o __init__.py
     install_requires=[
         'httpx',
         'rich'
     ],
     entry_points={
         'console_scripts': [
-            'moriarty=main:main', # Isso cria o comando nativo no terminal
+            'moriarty=src.main:main', # Aponta para o arquivo dentro de src e chama a função main()
         ],
     },
 )
